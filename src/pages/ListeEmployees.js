1ff09ac60eef2c35f4b2e449abcc7db844a268c4
Redavela <kodadreda67@gmail.com>
2021-11-27 17:57:25 +0100

    test

---
+++ src/pages/ListeEmployees.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { Link } from 'react-router-dom';
+
+const columns = [
+  { id: 'firstName', label: 'First Name' },
+  { id: 'lastName', label: 'Last Name' },
+  { id: 'startDate', label: 'Start Date' },
+  { id: 'department', label: 'Department' },
+  { id: 'birth', label: 'Date of Birth' },
+  { id: 'street', label: 'Street' },
+  { id: 'city', label: 'City' },
+  { id: 'state', label: 'State' },
+  { id: 'zipCode', label: 'Zip Code' },
+];
+
+function ListeEmployees() {
+  const listeEmployees =
+    JSON.parse(localStorage.getItem('listeEmployees')) || [];
+  const rows = listeEmployees.map((employee) => {
+    return {
+      ...employee,
+      startDate: employee.startDate.split('-').reverse().join('/'),
+      birth: employee.birth.split('-').reverse().join('/'),
+    };
+  });
+  console.log(rows);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+      <>
+      <h1>Current Employees</h1>
+    <Paper sx={{ width: '60%', margin:'auto', marginTop:'50', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 350 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+    <Link to="/">Home</Link>
+    </>
+  );
+}
+
+export default ListeEmployees;
---
+++ src/pages/Register.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { DropDown } from 'dropdownlib-reda';
+import { states, department } from '../DropDownData';
+
+const Register = () => {
+  const [employeeForm, setEmployeeForm] = useState({
+    firstName: '',
+    lastName: '',
+    birth: '',
+    startDate: '',
+    street: '',
+    city: '',
+    state: states[0].value,
+    zipCode: '',
+    department: department[0].value,
+  });
+
+  const handleChange = (e) => {
+    setEmployeeForm({
+      ...employeeForm,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let listeEmployees =
+      JSON.parse(localStorage.getItem('listeEmployees')) || [];
+    listeEmployees = [...listeEmployees, employeeForm];
+    localStorage.setItem('listeEmployees', JSON.stringify(listeEmployees));
+  };
+  return (
+    <main>
+      <div class="title">
+        <h1>HRnet</h1>
+      </div>
+      <div className="container">
+        <Link to="/liste">View Current Employees</Link>
+        <h2>Create Employee</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            value={employeeForm.firstName}
+            onChange={handleChange}
+            id="firstName"
+          />
+
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            value={employeeForm.lastName}
+            onChange={handleChange}
+            id="lastName"
+          />
+
+          <label htmlFor="date-of-birth">Date of Birth</label>
+          <input
+            id="birth"
+            value={employeeForm.birth}
+            onChange={handleChange}
+            type="date"
+          />
+
+          <label htmlFor="start-date">Start Date</label>
+          <input
+            id="startDate"
+            value={employeeForm.startDate}
+            onChange={handleChange}
+            type="date"
+          />
+
+          <fieldset className="address">
+            <legend>Address</legend>
+
+            <label htmlFor="street">Street</label>
+            <input
+              id="street"
+              value={employeeForm.street}
+              onChange={handleChange}
+              type="text"
+            />
+
+            <label htmlFor="city">City</label>
+            <input
+              id="city"
+              value={employeeForm.city}
+              onChange={handleChange}
+              type="text"
+            />
+
+            <DropDown
+              options={states}
+              id="state"
+              handleSelect={handleChange}
+              label="State"
+            />
+
+            <label htmlFor="zip-code">Zip Code</label>
+            <input
+              id="zipCode"
+              value={employeeForm.zipCode}
+              onChange={handleChange}
+              type="number"
+              required={false}
+            />
+          </fieldset>
+          <DropDown
+            options={department}
+            id="department"
+            handleSelect={handleChange}
+            label="Department"
+          />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
